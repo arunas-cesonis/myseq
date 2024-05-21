cb328977c91116d4a16585aa5a19b16e836e53cd
Arunas <karunce@gmail.com>
2024-05-21 21:34:56 +0000

    Add MySeq plugin

---
+++ plugins/MySeq/PluginUI.cpp
@@ -0,0 +1,69 @@
+/*
+ * Text Editor example
+ * Copyright (C) 2023 Filipe Coelho <falktx@falktx.com>
+ * SPDX-License-Identifier: ISC
+ */
+
+#include "DistrhoUI.hpp"
+
+START_NAMESPACE_DISTRHO
+
+// --------------------------------------------------------------------------------------------------------------------
+
+class MySeqUI : public UI
+{
+public:
+   /**
+      UI class constructor.
+      The UI should be initialized to a default state that matches the plugin side.
+    */
+    MySeqUI()
+        : UI(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT)
+    {
+        const double scaleFactor = getScaleFactor();
+
+        if (d_isEqual(scaleFactor, 1.0))
+        {
+            setGeometryConstraints(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT);
+        }
+        else
+        {
+            const uint width = DISTRHO_UI_DEFAULT_WIDTH * scaleFactor;
+            const uint height = DISTRHO_UI_DEFAULT_HEIGHT * scaleFactor;
+            setGeometryConstraints(width, height);
+            setSize(width, height);
+        }
+    }
+
+protected:
+    // ----------------------------------------------------------------------------------------------------------------
+    // DSP/Plugin Callbacks
+
+   /**
+      A parameter has changed on the plugin side.@n
+      This is called by the host to inform the UI about parameter changes.
+    */
+    void parameterChanged(uint32_t, float) override {}
+
+    void onImGuiDisplay() override {
+        ImGui::ShowMetricsWindow();
+    }
+
+    void stateChanged(const char* key, const char *value) override {
+
+    }
+    // ----------------------------------------------------------------------------------------------------------------
+
+    DISTRHO_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MySeqUI)
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UI* createUI()
+{
+    return new MySeqUI();
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+END_NAMESPACE_DISTRHO
